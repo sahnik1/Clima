dc27b441f23b00284f45601c2f95524fdfb0a093
Karanjot Singh Sahni <sahnik@mcmaster.ca>
2019-10-28 11:23:51 -0400

    Done, Only Some Visual Design Features Pending

--- clima/src/Views/HomeView.js
+++ clima/src/Views/HomeView.js
@@ -1,45 +1,81 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import * as material from '@material-ui/core';
+import ReactLoading from 'react-loading';
 
 class GetApi extends Component{
 
+    constructor(props) {
+        super(props);
+    
+        console.log(this.props)
+    }
+
     state = {
         loading: true,
-        weather: null,
-        temp: null
+        weather: null
     }
 
     async componentDidMount(){
         const proxy = `https://cors-anywhere.herokuapp.com/`;
         const access_key = ``;
-        const url = proxy + `http://api.weatherstack.com/current?access_key=${access_key}&query=fetch:ip`;
-        /* if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(position => {
-                let long = position.coords.longitude;
-                let lat = position.coords.latitude;
-                const url = proxy + `http://api.weatherstack.com/current?access_key=${access_key}&query=${lat},${long}`;
-            });
-        } */
+        const publicIp = await (require('public-ip')).v4();
+
+        let url = proxy + `http://api.weatherstack.com/current?access_key=${access_key}&query=${this.props.city}, ${this.props.country}`;
+        console.log(url);
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({weather: data, temp: data.current.temperature, loading: false});
+        console.log(data);
+        if (data){
+            this.setState({weather: data, loading: false});
+        }
     }
 
     render(){
         if(this.state.loading){
-            return (<div>
-                LOADING...
-            </div>)
-        }
-        if(!this.state.weather){
             return (
-                <div>An Unknown Error Occured!</div>
+            <div style={{background: `linear-gradient(#74b9ff, #00cec9)`, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3em', position: 'absolute', top: '10%', left: '42%'}}>
+                <ReactLoading
+                        type={"spinningBubbles"}
+                        color={'#1E3799'}
+                        height={392}
+                        width={221}
+                    />
+                </div>
+            </div>
+            )
+        }
+        if(!this.state.weather || this.state.weather.error){
+            return (
+                <div style={{background: `linear-gradient(#74b9ff, #00cec9)`, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3em', position: 'absolute', top: '10%', left: '42%'}}>
+                    An Unknown Error Occurred!
+                </div>
+            </div>
             )
         }
         return (
-            <div>
-                <h1>Temperature is: </h1>
-                <div>{this.state.temp} C</div>
+            <div style={{background: `linear-gradient(#74b9ff, #00cec9)`, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3em', position: 'absolute', top: '10%', left: '42%'}}>
+                <material.MuiThemeProvider>
+                <material.Grid container direction="column" justify="center" alignItems="center">
+                    <material.Grid xs={12} style={{alignContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
+                    <h1>
+                            <img src={this.state.weather.current.weather_icons[0]} style={{ fontSize: '30px'}}/>
+                            <span style={{ justifyContent: 'space-between', fontWeight: 375, fontSize: '50px' }}>
+                            {this.state.weather.location.name}
+                            </span>
+                    </h1>
+                    <span style={{ justifyContent: 'center', alignItems: 'center', top: '40%', fontWeight: 350, fontSize: '35px' }}>
+                    {this.state.weather.current.weather_descriptions[0]}
+                    </span>
+                <h3 style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3em', position: 'absolute', top: '15%', left: '25%', fontSize: '45px'}}>
+                {this.state.weather.current.temperature}&deg;C</h3>
+                </material.Grid>
+                </material.Grid>
+                </material.MuiThemeProvider>
+            </div>
             </div>
         );
     }
--- clima/src/index.js
+++ clima/src/index.js
@@ -13,10 +13,34 @@ import * as serviceWorker from './serviceWorker';
 
 class Index extends Component{
 
-    handleClick(){
+    constructor(props) {
+        super(props);
+        this.handleCity = this.handleCity.bind(this);
+        this.handleCountry = this.handleCountry.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    state = {
+        cityinfo: '',
+        countryinfo: ''
+    }
+
+    handleCity(event){
+        this.setState({cityinfo: event.target.value});
+    }
+
+    handleCountry(event){
+        this.setState({countryinfo: event.target.value});
+    }
+
+    handleSubmit(event){
+        console.log(this.state.cityinfo);
+        console.log(this.state.countryinfo);
+        event.preventDefault();
         render(
             <div>
-                <GetApi />
+                <GetApi country={this.state.countryinfo}
+                        city={this.state.cityinfo}/>
             </div>,
             document.getElementById('root')
         );
@@ -24,27 +48,40 @@ class Index extends Component{
 
     render(){
         return(
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '1em', color:'#3498db', position: 'absolute', top: '30%', left: '47%'}}>
-            <Sky
-                images={{}}
-            />
+        <div style={{background: `linear-gradient(#74b9ff, #00cec9)`, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3%', color:'#1E3799', position: 'absolute', top: '20%', left: '45%'}}>
             <material.MuiThemeProvider>
                 <material.Grid container direction="column" justify="center" alignItems="center">
                     <material.Grid xs={12} style={{alignContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
                         <h1>
-                            <CloudIcon style={{bottom: '-2px'}}/>
-                            <span style={{ justifyContent: 'space-between', left: '-50px', fontWeight: 350, fontSize: '35px' }}>
+                            <CloudIcon style={{ fontSize: '40px'}}/>
+                            <span style={{ justifyContent: 'space-between', fontWeight: 375, fontSize: '50px' }}>
                             Clima
                             </span>
                         </h1>
-                        <material.Button raised={true} primary={true} style={{top: '30px', backgroundColor: '#3498db'}} active onClick={this.handleClick}>
-                            <h6 style={{ color: '#ffffff' }}>
+
+                        <form onSubmit={this.handleSubmit}>
+                        <material.TextField label="City" type="text" id="City" value={this.state.cityinfo} 
+                        style={{ justifyContent: 'center', display: 'flex', alignItems: 'center', top: '10%', color: '#ffffff', textOverflow: 'clip', cursor: 'pointer', marginTop: '10%' }} 
+                        onChange={this.handleCity}/>
+
+                        <material.TextField label="Country" type="text" value={this.state.countryinfo} id="Country" 
+                        style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', top: '50%', color: '#ffffff', marginTop: '10%' }}
+                        onChange={this.handleCountry}/>
+
+                        <material.Fab variant="extended" size="medium" type="submit"
+                        style={{alignContent: 'center', display: 'flex', alignItems: 'center', top: '100%', backgroundColor: '#1E3799', marginTop: '20%'}}
+                        active onClick={this.handleClick} >
+                            <h6 style={{ color: '#dfe6e9' }}>
                                 Find My Weather
                             </h6>
-                        </material.Button>
+                        </material.Fab>
+                        </form>
+
                     </material.Grid>
                 </material.Grid>
             </material.MuiThemeProvider>
+            </div>
         </div>
         )
     }
