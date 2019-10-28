e6b04de096846ac6983ea006eb07644b86e4f7b3
Karanjot Singh Sahni <sahnik@mcmaster.ca>
2019-10-28 11:58:52 -0400

    Cleaning...

--- clima/src/Views/HomeView.js
+++ clima/src/Views/HomeView.js
@@ -18,7 +18,7 @@ class GetApi extends Component{
 
     async componentDidMount(){
         const proxy = `https://cors-anywhere.herokuapp.com/`;
-        const access_key = ``;
+        const access_key = `295b72e6d7a938836f057ff48092f037`;
         const publicIp = await (require('public-ip')).v4();
 
         let url = proxy + `http://api.weatherstack.com/current?access_key=${access_key}&query=${this.props.city}, ${this.props.country}`;
@@ -49,15 +49,15 @@ class GetApi extends Component{
         if(!this.state.weather || this.state.weather.error){
             return (
                 <div style={{background: `linear-gradient(#74b9ff, #00cec9)`, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
-                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3em', position: 'absolute', top: '10%', left: '42%'}}>
-                    An Unknown Error Occurred!
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0em', position: 'absolute', top: '10%', left: '30%', color: '#dfe6e9'}}>
+                <img src="https://cdn.dribbble.com/users/20580/screenshots/1276530/artisan-errors.gif" style={{ fontSize: '10px'}}/>
                 </div>
             </div>
             )
         }
         return (
             <div style={{background: `linear-gradient(#74b9ff, #00cec9)`, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}>
-                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '3em', position: 'absolute', top: '10%', left: '42%'}}>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0em', position: 'absolute', top: '10%', left: '42%'}}>
                 <material.MuiThemeProvider>
                 <material.Grid container direction="column" justify="center" alignItems="center">
                     <material.Grid xs={12} style={{alignContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
